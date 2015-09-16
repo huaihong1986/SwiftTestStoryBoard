da0674bc2530fbed3f53951bf728b1376b3ed757
huaihong1986 <huaihong1986@sina.com>
2015-09-16 12:57:37 +0800

    Initial Commit

---
+++ SwiftTestStoryBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTestStoryBoard
+//
+//  Created by Hu Aihong on 15-9-16.
+//  Copyright (c) 2015年 ChinaCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
